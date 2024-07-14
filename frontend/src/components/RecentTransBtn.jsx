9bf9adbee71727142a1bc6ead063ec0d7f380beb
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-07-14 19:40:09 +0700

    chore: Update RecentTransBtn component to display dynamic data

--- frontend/src/components/RecentTransBtn.jsx
+++ frontend/src/components/RecentTransBtn.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react/prop-types
-export default function RecentTransBtn({ img }) {
+export default function RecentTransBtn({ img, items, tag, amount, date }) {
   return (
     <>
       <div className="flex justify-between items-center py-6">
@@ -9,14 +9,14 @@ export default function RecentTransBtn({ img }) {
           </div>
           <div className="flex gap-1 flex-col">
             <div>
-              <h2 className="text-base font-bold capitalize">GTR 5</h2>
-              <p className="text-xs font-normal">Gadget & Gear </p>
+              <h2 className="text-base font-bold capitalize">{items}</h2>
+              <p className="text-xs font-normal capitalize">{tag}</p>
             </div>
           </div>
         </div>
         <div className="py-2 px-3">
-          <h1 className="font-semibold">$160.00</h1> 
-          <p className="text-xs">17 May 2023</p>
+          <h1 className="font-semibold">${amount}</h1> 
+          <p className="text-xs">{new Date(date).toLocaleDateString}</p>
         </div>
       </div>
     </>
--- frontend/src/components/RecentTransaction.jsx
+++ frontend/src/components/RecentTransaction.jsx
@@ -1,7 +1,47 @@
 import Game from "../assets/Game.svg";
 import RecentTransBtn from "./RecentTransBtn";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 export default function RecentTransaction() {
+  // eslint-disable-next-line no-unused-vars
+  const [isUser, setisUser] = useState({});
+  const { user } = useAuthContext();
+  const [isTransaction, setIsTransaction] = useState([]);
+
+  useEffect(() => {
+    if (user && user.token) {
+      const decoded = jwtDecode(user.token);
+      axios
+        .get(`http://localhost:5000/user/${decoded._id}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        })
+        .then((response) => {
+          setisUser(response.data);
+          axios
+            .get(`http://localhost:5000/transaction?user_id=${decoded._id}`, {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+            })
+            .then((response) => {
+              console.log("Transaction Response:", response.data);
+              setIsTransaction(response.data.data);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [user]);
+
   return (
     <div className="flex flex-col w-[365px] pt-6">
       <h1 className="text-neutral text-[22px]">Recent Transaction</h1>
@@ -12,12 +52,17 @@ export default function RecentTransaction() {
           <h3>Expenses</h3>
         </div>
         <div>
-          <RecentTransBtn img={Game} />
-          <RecentTransBtn img={Game} />
-          <RecentTransBtn img={Game} />
-          <RecentTransBtn img={Game} />
-          <RecentTransBtn img={Game} />
-        </div> 
+          {isTransaction.slice(0, 6).map((transaction) => (
+            <RecentTransBtn
+              key={transaction._id}
+              img={Game}
+              items={transaction.items}
+              tag={transaction.tag}
+              amount={transaction.amount}
+              date={transaction.date}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
