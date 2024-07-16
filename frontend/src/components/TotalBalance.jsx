ff4112d8045dc1cbc5d92777726a96d718578d79
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-07-16 19:13:31 +0700

    chore: Update TotalBalance component to fetch user balance data and display dynamically

--- frontend/src/components/TotalBalance.jsx
+++ frontend/src/components/TotalBalance.jsx
@@ -1,21 +1,60 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import Mastercard from "../assets/Mastercard.svg";
 import Pagination from "./Pagination";
-import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 export default function TotalBalance() {
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 3; // Set this to the total number of pages you have
+  const [isUser, setIsUser] = useState({});
+  const { user } = useAuthContext();
+  const [balance, setBalance] = useState([]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
+  const currentData = balance[currentPage - 1];
+
+  useEffect(() => {
+    if (user && user.token) {
+      const decoded = jwtDecode(user.token);
+      axios
+        .get(`http://localhost:5000/user/${decoded._id}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        })
+        .then((response) => {
+          setIsUser(response.data);
+
+          return axios.get(`http://localhost:5000/balances`, {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          });
+        })
+        .then((response) => {
+          setBalance(response.data.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [user]);
+
+  if (!currentData) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <h1 className="text-neutral text-[22px]">Total Balance</h1>
       <div className="py-5 px-6 bg-white shadow-md h-[232px]">
         <div className="flex justify-between">
-          <h1 className="text-[22px] font-extrabold">$240,399</h1>
+          <h1 className="text-[22px] font-extrabold">{currentData.balance}</h1>
           <p className="text-base font-medium">All Accounts</p>
         </div>
         <div className="bg-primary p-4 mt-3 text-white">
@@ -23,10 +62,12 @@ export default function TotalBalance() {
             <h1 className="text-base font-medium">Account type</h1>
             <img src={Mastercard} alt="" />
           </div>
-          <h2 className="text-base font-bold">Credit Card</h2>
+          <h2 className="text-base font-bold">{currentData.type}</h2>
           <div className="flex gap-2">
-            <h3 className="text-base font-medium">**** **** **** 2598</h3>
-            <p className="text-base font-bold w-[92px] text-right">$25000 </p>
+            <h3 className="text-base font-medium">{currentData.acc_number}</h3>
+            <p className="text-base font-bold w-[92px] text-right">
+              ${new Number(currentData.balance).toLocaleString()}
+            </p>
             <svg
               width="24"
               height="24"
@@ -55,7 +96,7 @@ export default function TotalBalance() {
         <div className="flex justify-center mt-1 mb-1">
           <Pagination
             currentPage={currentPage}
-            totalPages={totalPages}
+            totalPages={balance.length}
             onPageChange={handlePageChange}
           />
         </div>
